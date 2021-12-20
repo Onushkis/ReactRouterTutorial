40e1120fc2dd41d18729c387b4704d78279bd06b
Ona <onushkis@gmail.com>
2021-12-20 14:27:12 +0100

    add router tutorial Done

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import react from "react";
+import ReactDOM from 'react-dom';
+import { BrowserRouter,  Routes, Route } from "react-router-dom";
+// import { render } from "react-dom";
+import App from "./App";
+import Expenses from "./Routes/Expenses";
+import Invoices from "./Routes/Invoices";
+import Invoice from "../src/Routes/Invoice";
+
+
+const rootElement = document.getElementById('app');
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />}>
+        <Route
+          index
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>Select an invoice</p>
+            </main>
+          }
+        />
+        <Route path=":invoiceId" element={<Invoice />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Route>
+  </Routes>
+</BrowserRouter>,
+  rootElement
+);
+
+
+
+
